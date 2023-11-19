5b4e8d172e75a6c493ee76ea8abc42f3c8a319fe
Dan Wahl <drwahl@gmail.com>
2023-11-19 15:43:27 -0600

    Separating web App out to remove Platform dependencies, native code from web bundling

--- App.web.tsx
+++ App.web.tsx
@@ -3,19 +3,15 @@ import { useState, useEffect } from "react";
 import { View } from "react-native";
 import { ActivityIndicator, PaperProvider } from "react-native-paper";
 import { supabase } from "./lib/supabase";
-import { Session } from "@supabase/supabase-js";
 import Router from "./components/Router";
+import { Session } from "@supabase/supabase-js";
 import {
   useFonts,
   EduNSWACTFoundation_400Regular,
   EduNSWACTFoundation_500Medium,
 } from "@expo-google-fonts/edu-nsw-act-foundation";
-import { StatusBar } from "expo-status-bar";
 import { useStyles } from "./lib/styles";
 import theme from "./lib/theme";
-import { LogLevel, OneSignal } from "react-native-onesignal";
-import Constants from "expo-constants";
-import * as Device from "expo-device";
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -47,32 +43,8 @@ export default function App() {
           setSession(session);
           break;
       }
-
-      if (Device.isDevice) {
-        OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-        OneSignal.initialize(Constants.expoConfig?.extra?.oneSignalAppId);
-        // TODO(drw): use OneSignal.Notifications.addEventListener to handle notifications when app in focus
-        // OneSignal.Notifications.addEventListener(
-        //   "foregroundWillDisplay",
-        //   (event) => {
-        //     event.preventDefault();
-        //     event.getNotification().display();
-        //   }
-        // );
-      }
     });
   }, []);
-
-  useEffect(() => {
-    if (Device.isDevice) {
-      if (session?.user.id) {
-        OneSignal.login(session.user.id);
-        OneSignal.Notifications.requestPermission(true);
-      } else {
-        OneSignal.logout();
-      }
-    }
-  }, [session?.user.id]);
 
   if (loading || (!fontsLoaded && !fontError)) {
     return (
@@ -85,7 +57,6 @@ export default function App() {
   return (
     <PaperProvider theme={theme}>
       <View style={styles.appView}>
-        <StatusBar style="auto" />
         <Router session={session} />
       </View>
     </PaperProvider>
