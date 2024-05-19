72deb9c295b261370608d5d038b7365ee978140b
Dan Wahl <drwahl@gmail.com>
2024-05-19 11:35:51 -0500

    Expo upgrade (#92)
    * Misc. cleanup, bumping version
    
    * Upgrading to Expo 50
    
    * Upgrading to Expo 51
    
    * Fixing VirtualizedList error by replacing FlatLists with maps

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
     // https://github.com/expo/expo/issues/20456#issuecomment-1707648241
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
-      "expo-router/babel",
       "module:react-native-dotenv",
       "react-native-reanimated/plugin",
     ],
